55ad4f1da9c53de6ef68f625f4653ea492798f4b
ssuttles-usgs <ssuttles@usgs.gov>
2021-03-25 10:48:33 -0400

    update continuous sampling code

--- src/sensors/VaisalaWXT536R.h
+++ src/sensors/VaisalaWXT536R.h
@@ -19,29 +19,29 @@
 #include "sensors/SDI12SensorsR.h"
 
 // Sensor Specific Defines
-#define WXTR_NUM_VARIABLES 6
-#define WXTR_WARM_UP_TIME_MS 500
-#define WXTR_STABILIZATION_TIME_MS 0
+#define WXT_NUM_VARIABLES 6
+#define WXT_WARM_UP_TIME_MS 500
+#define WXT_STABILIZATION_TIME_MS 0
 // #define WXT_MEASUREMENT_TIME_MS 12500
-#define WXTR_MEASUREMENT_TIME_MS 3350 // instrument says it returns in 61 seconds, but give it a little extra
+#define WXT_MEASUREMENT_TIME_MS 3350 // instrument says it returns in 61 seconds, but give it a little extra
 
-#define WXTR_DM_RESOLUTION 0
-#define WXTR_DM_VAR_NUM 0
+#define WXT_DM_RESOLUTION 0
+#define WXT_DM_VAR_NUM 0
 
-#define WXTR_SM_RESOLUTION 1
-#define WXTR_SM_VAR_NUM 1
+#define WXT_SM_RESOLUTION 1
+#define WXT_SM_VAR_NUM 1
 
-#define WXTR_TA_RESOLUTION 1
-#define WXTR_TA_VAR_NUM 2
+#define WXT_TA_RESOLUTION 1
+#define WXT_TA_VAR_NUM 2
 
-#define WXTR_UA_RESOLUTION 1
-#define WXTR_UA_VAR_NUM 3
+#define WXT_UA_RESOLUTION 1
+#define WXT_UA_VAR_NUM 3
 
-#define WXTR_PA_RESOLUTION 1
-#define WXTR_PA_VAR_NUM 4
+#define WXT_PA_RESOLUTION 1
+#define WXT_PA_VAR_NUM 4
 
-#define WXTR_RC_RESOLUTION 2
-#define WXTR_RC_VAR_NUM 5
+#define WXT_RC_RESOLUTION 2
+#define WXT_RC_VAR_NUM 5
 
 // The main class for the Vaisala WXT520
 class VaisalaWXT536R : public SDI12SensorsR
@@ -50,18 +50,18 @@ public:
     // Constructors with overloads
     VaisalaWXT536R(char SDI12address, int8_t powerPin, int8_t dataPin, uint8_t measurementsToAverage = 1)
      : SDI12SensorsR(SDI12address, powerPin, dataPin, measurementsToAverage,
-                    "VaisalaWXT536R", WXTR_NUM_VARIABLES,
-                    WXTR_WARM_UP_TIME_MS, WXTR_STABILIZATION_TIME_MS, WXTR_MEASUREMENT_TIME_MS)
+                    "VaisalaWXT536R", WXT_NUM_VARIABLES,
+                    WXT_WARM_UP_TIME_MS, WXT_STABILIZATION_TIME_MS, WXT_MEASUREMENT_TIME_MS)
     {}
     VaisalaWXT536R(char *SDI12address, int8_t powerPin, int8_t dataPin, uint8_t measurementsToAverage = 1)
      : SDI12SensorsR(SDI12address, powerPin, dataPin, measurementsToAverage,
-                    "VaisalaWXT536R", WXTR_NUM_VARIABLES,
-                    WXTR_WARM_UP_TIME_MS, WXTR_STABILIZATION_TIME_MS, WXTR_MEASUREMENT_TIME_MS)
+                    "VaisalaWXT536R", WXT_NUM_VARIABLES,
+                    WXT_WARM_UP_TIME_MS, WXT_STABILIZATION_TIME_MS, WXT_MEASUREMENT_TIME_MS)
     {}
     VaisalaWXT536R(int SDI12address, int8_t powerPin, int8_t dataPin, uint8_t measurementsToAverage = 1)
      : SDI12SensorsR(SDI12address, powerPin, dataPin, measurementsToAverage,
-                    "VaisalaWXT536R", WXTR_NUM_VARIABLES,
-                    WXTR_WARM_UP_TIME_MS, WXTR_STABILIZATION_TIME_MS, WXTR_MEASUREMENT_TIME_MS)
+                    "VaisalaWXT536R", WXT_NUM_VARIABLES,
+                    WXT_WARM_UP_TIME_MS, WXT_STABILIZATION_TIME_MS, WXT_MEASUREMENT_TIME_MS)
     {}
     // Destructor
     ~VaisalaWXT536R(){}
@@ -71,19 +71,19 @@ public:
 class VaisalaWXT536R_Dm : public Variable
 {
 public:
-    VaisalaWXT536R_Dm(SensorR *parentSense,
+    VaisalaWXT536R_Dm(Sensor *parentSense,
                     const char *uuid = "",
-                    const char *varCode = "WXTRDm")
+                    const char *varCode = "WXTDm")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_DM_VAR_NUM,
-                 (uint8_t)WXTR_DM_RESOLUTION,
+                 (const uint8_t)WXT_DM_VAR_NUM,
+                 (uint8_t)WXT_DM_RESOLUTION,
                  "direction", "degrees",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Dm()
-      : Variable((const uint8_t)WXTR_DM_VAR_NUM,
-                 (uint8_t)WXTR_DM_RESOLUTION,
-                 "direction", "degrees", "WXTRDm")
+      : Variable((const uint8_t)WXT_DM_VAR_NUM,
+                 (uint8_t)WXT_DM_RESOLUTION,
+                 "direction", "degrees", "WXTDm")
     {}
     ~VaisalaWXT536R_Dm(){}
 };
@@ -92,19 +92,19 @@ public:
 class VaisalaWXT536R_Sm : public Variable
 {
 public:
-    VaisalaWXT536R_Sm(SensorR *parentSense,
+    VaisalaWXT536R_Sm(Sensor *parentSense,
                     const char *uuid = "",
-                    const char *varCode = "WXTRSm")
+                    const char *varCode = "WXTSm")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_SM_VAR_NUM,
-                 (uint8_t)WXTR_SM_RESOLUTION,
+                 (const uint8_t)WXT_SM_VAR_NUM,
+                 (uint8_t)WXT_SM_RESOLUTION,
                  "speed", "metersPerSecond",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Sm()
-      : Variable((const uint8_t)WXTR_SM_VAR_NUM,
-                 (uint8_t)WXTR_SM_RESOLUTION,
-                 "speed", "metersPerSecond", "WXTRSm")
+      : Variable((const uint8_t)WXT_SM_VAR_NUM,
+                 (uint8_t)WXT_SM_RESOLUTION,
+                 "speed", "metersPerSecond", "WXTSm")
     {}
     ~VaisalaWXT536R_Sm(){}
 };
@@ -114,19 +114,19 @@ public:
 class VaisalaWXT536R_Pa : public Variable
 {
 public:
-    VaisalaWXT536R_Pa(SensorR *parentSense,
+    VaisalaWXT536R_Pa(Sensor *parentSense,
                      const char *uuid = "",
-                     const char *varCode = "WXTRPa")
+                     const char *varCode = "WXTPa")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_PA_VAR_NUM,
-                 (uint8_t)WXTR_PA_RESOLUTION,
+                 (const uint8_t)WXT_PA_VAR_NUM,
+                 (uint8_t)WXT_PA_RESOLUTION,
                  "airPressure", "hPa",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Pa()
-      : Variable((const uint8_t)WXTR_PA_VAR_NUM,
-                 (uint8_t)WXTR_PA_RESOLUTION,
-                 "airPressure", "hPa", "WXTRPa")
+      : Variable((const uint8_t)WXT_PA_VAR_NUM,
+                 (uint8_t)WXT_PA_RESOLUTION,
+                 "airPressure", "hPa", "WXTPa")
     {}
     ~VaisalaWXT536R_Pa(){}
 };
@@ -135,19 +135,19 @@ public:
 class VaisalaWXT536R_Ta : public Variable
 {
 public:
-    VaisalaWXT536R_Ta(SensorR *parentSense,
+    VaisalaWXT536R_Ta(Sensor *parentSense,
                     const char *uuid = "",
-                    const char *varCode = "WXTRTa")
+                    const char *varCode = "WXTTa")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_TA_VAR_NUM,
-                 (uint8_t)WXTR_TA_RESOLUTION,
+                 (const uint8_t)WXT_TA_VAR_NUM,
+                 (uint8_t)WXT_TA_RESOLUTION,
                  "airTemperature", "degreeCelsius",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Ta()
-      : Variable((const uint8_t)WXTR_TA_VAR_NUM,
-                 (uint8_t)WXTR_TA_RESOLUTION,
-                 "airTemperature", "degreeCelsius", "WXTRTa")
+      : Variable((const uint8_t)WXT_TA_VAR_NUM,
+                 (uint8_t)WXT_TA_RESOLUTION,
+                 "airTemperature", "degreeCelsius", "WXTTa")
     {}
     ~VaisalaWXT536R_Ta(){}
 };
@@ -156,19 +156,19 @@ public:
 class VaisalaWXT536R_Ua : public Variable
 {
 public:
-    VaisalaWXT536R_Ua(SensorR *parentSense,
+    VaisalaWXT536R_Ua(Sensor *parentSense,
                      const char *uuid = "",
-                     const char *varCode = "WXTRUa")
+                     const char *varCode = "WXTUa")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_UA_VAR_NUM,
-                 (uint8_t)WXTR_UA_RESOLUTION,
+                 (const uint8_t)WXT_UA_VAR_NUM,
+                 (uint8_t)WXT_UA_RESOLUTION,
                  "relativeHumidity", "percent",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Ua()
-      : Variable((const uint8_t)WXTR_UA_VAR_NUM,
-                 (uint8_t)WXTR_UA_RESOLUTION,
-                 "relativeHumidity", "percent", "WXTRUa")
+      : Variable((const uint8_t)WXT_UA_VAR_NUM,
+                 (uint8_t)WXT_UA_RESOLUTION,
+                 "relativeHumidity", "percent", "WXTUa")
     {}
     ~VaisalaWXT536R_Ua(){}
 };
@@ -180,17 +180,17 @@ class VaisalaWXT536R_Rc : public Variable
 public:
     VaisalaWXT536R_Rc(Sensor *parentSense,
                     const char *uuid = "",
-                    const char *varCode = "WXTRRc")
+                    const char *varCode = "WXTRc")
       : Variable(parentSense,
-                 (const uint8_t)WXTR_RC_VAR_NUM,
-                 (uint8_t)WXTR_RC_RESOLUTION,
+                 (const uint8_t)WXT_RC_VAR_NUM,
+                 (uint8_t)WXT_RC_RESOLUTION,
                  "rainfall", "mm",
                  varCode, uuid)
     {}
     VaisalaWXT536R_Rc()
-      : Variable((const uint8_t)WXTR_RC_VAR_NUM,
-                 (uint8_t)WXTR_RC_RESOLUTION,
-                 "rainfall", "mm", "WXTRRc")
+      : Variable((const uint8_t)WXT_RC_VAR_NUM,
+                 (uint8_t)WXT_RC_RESOLUTION,
+                 "rainfall", "mm", "WXTRc")
     {}
     ~VaisalaWXT536R_Rc(){}
 };
